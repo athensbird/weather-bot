2be94f43313b7e603b27d5c91f2cf62419a280a4
athensbird <popularwatson@gmail.com>
2018-01-14 01:29:52 -0700

    update folder name

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/Mark/Documents/HackArizona/bot/venv"
+set -gx VIRTUAL_ENV "/Users/Mark/Documents/HackArizona/weather-bot/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
